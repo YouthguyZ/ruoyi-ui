b37bdc5c2003825512021adbfa7a66acfbeff0c8
ahaoo <2281300996@qq.com>
2024-07-29 18:05:12 +0800

    init

---
+++ src/views/system/attachment/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <!-- -->
+    <h1>最低附件张数</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RuoyiUiIndex',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+};
+</script>
